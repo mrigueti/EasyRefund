4e7c410aa8b72eb9a9b499ae8e1ad001c50ad55a
Gabryel-Modesto <gabryel.modesto@aluno.senai.br>
2024-11-04 09:49:57 -0300

    Finalizando a página de envio de comprovante

--- src/components/UploadDocument/UploadDocumentComponent.jsx
+++ src/components/UploadDocument/UploadDocumentComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Container, Row, Col, Form, Button, ListGroup } from "react-bootstrap";
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Form, Button, ListGroup, Modal } from "react-bootstrap";
 import { XCircleFill } from "react-bootstrap-icons";
 import styles from "./UploadDocumentComponent.module.css";
 import { useNavigate } from "react-router-dom";
@@ -7,9 +7,12 @@ import { useNavigate } from "react-router-dom";
 export default function UploadDocument() {
   const navigate = useNavigate();
   const [files, setFiles] = useState([]);
-  const [showTextArea, setShowTextArea] = useState(""); // Controla qual textarea mostrar
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // Estado para controle de desativação dos botões
-  const textAreaRef = useRef(null); // Ref para o textarea
+  const componentRef = useRef(null);
+  const [selectedButton, setSelectedButton] = useState(null);
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showModal, setShowModal] = useState(false); // Estado para o modal de confirmação
+  const fileInputRef = useRef(null); // Referência para o input de arquivo
 
   const handleFileChange = (event) => {
     if (event.target.files) {
@@ -28,31 +31,54 @@ export default function UploadDocument() {
     navigate(-1);
   };
 
-  const handleShowTextArea = (type) => {
-    setShowTextArea(type); // Atualiza o estado para o tipo correspondente
-    setIsButtonDisabled(true); // Desativa os outros botões
+  const handleClickSelected = (button) => {
+    setSelectedButton(button);
   };
 
-  // Efeito para lidar com cliques fora do textarea
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (textAreaRef.current && !textAreaRef.current.contains(event.target)) {
-        setShowTextArea(""); // Oculta o textarea se o clique for fora
-        setIsButtonDisabled(false); // Reativa os botões
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const errors = []; // Array para armazenar mensagens de erro
+
+    // Verificação se não há arquivos ou categoria não selecionada
+    if (files.length === 0) {
+      errors.push("Por favor, anexe um arquivo.");
+    }
+    if (!selectedButton) {
+      errors.push("Por favor, selecione uma categoria.");
+    }
+
+    // Se houver erros, atualiza o estado de mensagens de erro
+    if (errors.length > 0) {
+      setErrorMessages(errors);
+      setSuccessMessage(""); // Limpa a mensagem de sucesso, se houver
+    } else {
+      setErrorMessages([]); // Limpa as mensagens de erro
+      setSuccessMessage("Envio realizado com sucesso!"); // Define a mensagem de sucesso
+
+      // Limpa os campos após o envio
+      setFiles([]);
+      setSelectedButton(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = ""; // Limpa o input de arquivo
       }
-    };
+      setShowModal(true); // Mostra o modal após o envio bem-sucedido
+    }
+  };
 
-    // Adiciona o listener ao clique do documento
-    document.addEventListener("mousedown", handleClickOutside);
-
-    // Limpa o listener ao desmontar o componente
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const handleCloseModal = (sendAnother) => {
+    setShowModal(false); // Fecha o modal
+    if (sendAnother) {
+      // Se o usuário deseja enviar outro documento, não faz nada
+      setSuccessMessage(""); // Limpa a mensagem de sucesso
+    } else {
+      // Se o usuário não deseja enviar outro documento, volta à página anterior
+      handleBtnBackPage();
+    }
+  };
 
   return (
-    <div className={styles.component}>
+    <div ref={componentRef} className={styles.component}>
       <button
         className={`${styles.infoButtonBack} ${styles.button_back_position}`}
         onClick={handleBtnBackPage}
@@ -63,67 +89,58 @@ export default function UploadDocument() {
       <Container>
         <Row className="justify-content-center">
           <Col md={6}>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3">
                 <Form.Control
                   type="file"
                   onChange={handleFileChange}
                   multiple
+                  ref={fileInputRef} // Adicionando a referência aqui
                 />
-                <div className={styles.filterBtns}>
-                  <div className={styles.buttonWithTextArea}>
-                    <button 
-                      className={styles.btnHotel} 
-                      onClick={(e) => { e.preventDefault(); handleShowTextArea('hotel'); }}
-                      disabled={isButtonDisabled} // Desativa o botão se isButtonDisabled for true
-                    >
-                      Hotel
-                    </button>
-                    {showTextArea === 'hotel' && (
-                      <textarea 
-                        ref={textAreaRef}
-                        name="textAreaHotel" 
-                        id="textAreaHotel" 
-                        className={styles.textareaLarge} 
-                      ></textarea>
-                    )}
-                  </div>
-                  <div className={styles.buttonWithTextArea}>
-                    <button 
-                      className={styles.btnFood} 
-                      onClick={(e) => { e.preventDefault(); handleShowTextArea('food'); }}
-                      disabled={isButtonDisabled} // Desativa o botão se isButtonDisabled for true
-                    >
-                      Comida
-                    </button>
-                    {showTextArea === 'food' && (
-                      <textarea 
-                        ref={textAreaRef}
-                        name="textAreaFood" 
-                        id="textAreaFood" 
-                        className={styles.textareaLarge} 
-                      ></textarea>
-                    )}
-                  </div>
-                  <div className={styles.buttonWithTextArea}>
-                    <button 
-                      className={styles.btnTransport} 
-                      onClick={(e) => { e.preventDefault(); handleShowTextArea('transport'); }}
-                      disabled={isButtonDisabled} // Desativa o botão se isButtonDisabled for true
-                    >
-                      Transporte
-                    </button>
-                    {showTextArea === 'transport' && (
-                      <textarea 
-                        ref={textAreaRef}
-                        name="textAreaTransport" 
-                        id="textAreaTransport" 
-                        className={styles.textareaLarge} 
-                      ></textarea>
-                    )}
-                  </div>
-                </div>
+                {errorMessages.includes("Por favor, anexe um arquivo.") && (
+                  <div className="text-danger">{errorMessages.find(msg => msg === "Por favor, anexe um arquivo.")}</div>
+                )}
               </Form.Group>
+              <div className={styles.BtnOption}>
+                <div>
+                  <button
+                    className={`${styles.BtnHotel} ${selectedButton === "hotel" ? styles.selected : ""}`}
+                    type="button"
+                    onClick={() => handleClickSelected("hotel")}
+                  >
+                    Hotel
+                  </button>
+                </div>
+                <div>
+                  <button
+                    className={`${styles.BtnFood} ${selectedButton === "food" ? styles.selected : ""}`}
+                    type="button"
+                    onClick={() => handleClickSelected("food")}
+                  >
+                    Comida
+                  </button>
+                </div>
+                <div>
+                  <button
+                    className={`${styles.BtnTransport} ${selectedButton === "transport" ? styles.selected : ""}`}
+                    type="button"
+                    onClick={() => handleClickSelected("transport")}
+                  >
+                    Transporte
+                  </button>
+                </div>
+              </div>
+              {errorMessages.includes("Por favor, selecione uma categoria.") && (
+                <div className="text-danger">{errorMessages.find(msg => msg === "Por favor, selecione uma categoria.")}</div>
+              )}
+              <div className={styles.TextArea}>
+                <textarea
+                  className={styles.textAreaDescription}
+                  name="textAreaDescription"
+                  id="textAreaDescription"
+                  placeholder="Descrição..."
+                ></textarea>
+              </div>
               {files.length > 0 && (
                 <ListGroup className="mb-3">
                   {files.map((file, index) => (
@@ -143,15 +160,36 @@ export default function UploadDocument() {
                   ))}
                 </ListGroup>
               )}
-              <div className={styles.button_container}>
-                <Button className={styles.button_refund} type="submit">
-                  Enviar
-                </Button>
+              <div className={styles.BtnSendDiv}>
+                <button className={styles.BtnSend} type="submit">Enviar</button>
               </div>
+              {successMessage && (
+                <div className="text-success mt-3 text-center">
+                  {successMessage}
+                </div>
+              )}
             </Form>
           </Col>
         </Row>
       </Container>
+
+      {/* Modal de confirmação */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Deseja enviar outro documento?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Você gostaria de enviar outro documento?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleCloseModal(false)}>
+            Não
+          </Button>
+          <Button variant="primary" onClick={() => handleCloseModal(true)}>
+            Sim
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
